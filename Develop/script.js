3d4aacfcedf1bfbd735a66dc251b7f2795193aa8
Jose Cotto <josecotto12@gmail.com>
2023-02-10 17:15:15 -0500

    Add all needed variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,24 @@
 // Assignment code here
-  // var UPPERCASE = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  // var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-// Create function that can be called when button is clicked
-function generatePassword(){ 
-  // Started the function by adding a prompt that requires the desired length of the password and if statements that gauge the responses.
-  var length = Number(prompt('How many characters will your password be? Enter a number between 8 and 128.'));
-
-  if (length <= 128) {
-    var uppercase = confirm('Do you want UPPERCASE letters in your password?');
-
-  } else if (length > 128) {
-    alert('I cant count that high!');
-  }
-  
-  else {
-    alert('Wrong Characters used silly');
-  }
-
-
-    // console.log ('Hey, You clicked the button!')
-    // prompt('How long do want your password (8-128)?');
-    // 1. prompt the user for the password criteria
-      // a. length 8 - 128s
-      // b. uppercase, lowercase, numbers , special characters.
-    // 2. validate input
-    // 3. password is displayed
-
-
-
-
-    return 'Generated password will go here!';
-  }
-
-// Get references to the #generate element
-// targeting the ID generate in the button element.
 var generateBtn = document.querySelector("#generate");
+var passMin = 8;
+var passMax = 128;
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", ":", ";", ",", ".", "<", ">", "?", "/", "`", "~"];
+var result;
+
+
+function generatePassword() {
+  var passLength = prompt('How long would you like your password? (8-128)');
+  if (passLength >= passMin && passLength <= passMax) {
+    alert('Your password will be ' + passLength + ' characters long')
+    var upperCase = confirm('Would you like your password to contain lowercase letters?');
+  } else {
+    alert('Error, Please use a valid number');
+
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -45,5 +29,20 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button. When button is clicked, then run write password function.
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// when button is pressed , then I am presented with prompts for password criteria.
+  // 1. length of password 8 - 128.
+  // 2. user chooses wether or not to include lowercase, uppercase, numeric and/or special values.
+  // when answering each prompt , the input type should be validated and at least one character type should be selected.
+  // when all prompts are answered, password is generated that meets the chosen criteria
+  // password is displayed in an alert or in the text field.
+
+
